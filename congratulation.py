2120c5ef8bb1541a1c6e39c6a273ad81649a02d7
me2015gojo <iammjgod@gmail.com>
2026-03-20 21:46:37 +0530

    initial commit

---
+++ congratulation.py
@@ -0,0 +1,8 @@
+c= int(input('enter type of congratulations'))
+a= 'happybirthday'
+b= 'normal'
+# Use 1 or 2 for normal and happybirthday
+if a:
+    print('Happy birthday to you')
+if b:
+    print('congratulations very much')
