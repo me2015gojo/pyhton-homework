c0001b9d1cab01b7810850f4f023d6dead598ed0
me2015gojo <iammjgod@gmail.com>
2026-04-02 12:30:53 +0530

    initial commit

---
+++ agecheccker.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age: "))
+
+
+if age >= 10 and age <= 18:
+    print("Welcome You can join in the class ")
+else:
+    print("Sorry You cannot join in this class you are older than 18")
