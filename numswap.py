b064e477a68dd0b546113fe08a2ee242685f2d6f
me2015gojo <iammjgod@gmail.com>
2026-04-02 12:25:39 +0530

    initial commit

---
+++ numswap.py
@@ -0,0 +1,15 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+
+temp = a   
+a = b      
+b = c      
+c = temp   
+
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
