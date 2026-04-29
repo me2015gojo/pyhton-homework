6760838c2e1e8a010d9e2f5b9c25c6c3ac5b7232
me2015gojo <iammjgod@gmail.com>
2026-04-29 11:23:30 +0530

    initial commit

---
+++ dueamm.py
@@ -0,0 +1,4 @@
+totalbill=float(input("enter your total bill:"))
+amountpaid=float(input("enter the amount paid:")) #amount paid is larger than total bill
+amount_given=amountpaid-totalbill
+print(amount_given) 
