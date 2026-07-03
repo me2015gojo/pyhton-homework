947bea9ee8fab9ab3dc42caac545ef4444dfe8f0
me2015gojo <iammjgod@gmail.com>
2026-07-03 11:17:46 +0530

    initial commit

---
+++ geomentryb.py
@@ -0,0 +1,8 @@
+import math
+
+angle_degrees = float(input())
+angle_radians = math.radians(angle_degrees)
+
+print(math.sin(angle_radians))
+print(math.cos(angle_radians))
+print(math.tan(angle_radians))
